27c823b47d6f0dd09bee8168e7c8ece88db47418
Никита Тарасов` <nikita.taras0ff@yandex.ru>
2022-08-05 16:01:41 +0300

    Revert "Revert "1. Changing the name "file" to "photo"""
    This reverts commit 8d4e0369c6407a8b9dd5a55cdeca868c5beda78e.

--- src/main/java/com/ntarasov/store/city/model/CityDoc.java
+++ src/main/java/com/ntarasov/store/city/model/CityDoc.java
@@ -5,6 +5,9 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Document
 @Getter
 @Setter
@@ -15,8 +18,9 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 public class CityDoc {
     @Id
-        private ObjectId id;
-        private String  name;
-        private String  minTimeDelivery;
-        private String  priceDelivery;
+    private ObjectId id;
+    private String name;
+    private String minTimeDelivery;
+    private String priceDelivery;
+    private Map<ObjectId, Float> prices = new HashMap<>();
 }
